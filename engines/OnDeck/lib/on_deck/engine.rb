6000d3ae77043c043bfa3985b5fae559d74cd372
Jaci <jaci.brunning@gmail.com>
2019-04-24 11:35:29 +0800

    Only load OnDeck schedule in prod

--- engines/OnDeck/lib/on_deck/engine.rb
+++ engines/OnDeck/lib/on_deck/engine.rb
@@ -11,12 +11,14 @@ module OnDeck
       Sidekiq::Scheduler.enabled = true
       Sidekiq::Scheduler.dynamic = true
 
-      Sidekiq.configure_server do |cfg|
-        cfg.on(:startup) do
-          puts "Registering Sidekiq Schedule for On Deck..."
-          Sidekiq.set_schedule('ondeck_update_recommendations', { cron: '* * * * *', class: 'OnDeck::UpdateRecommendationsJob' })  # Every 1 Minutes
-          Sidekiq.set_schedule('ondeck_update_gds', { cron: '*/5 * * * *', class: 'OnDeck::UpdateTbaAllGdsJob' }) # Every 5 Minutes
-          Sidekiq.set_schedule('ondeck_update_pps', { cron: '0 0 * * *', class: 'OnDeck::UpdateTbaAllPpsJob' })   # Every day, at midnight. Large query (~200 API requests to TBA)
+      unless Rails.env.development?
+        Sidekiq.configure_server do |cfg|
+          cfg.on(:startup) do
+            puts "Registering Sidekiq Schedule for On Deck..."
+            Sidekiq.set_schedule('ondeck_update_recommendations', { cron: '* * * * *', class: 'OnDeck::UpdateRecommendationsJob' })  # Every 1 Minutes
+            Sidekiq.set_schedule('ondeck_update_gds', { cron: '*/5 * * * *', class: 'OnDeck::UpdateTbaAllGdsJob' }) # Every 5 Minutes
+            Sidekiq.set_schedule('ondeck_update_pps', { cron: '0 0 * * *', class: 'OnDeck::UpdateTbaAllPpsJob' })   # Every day, at midnight. Large query (~200 API requests to TBA)
+          end
         end
       end
     end
